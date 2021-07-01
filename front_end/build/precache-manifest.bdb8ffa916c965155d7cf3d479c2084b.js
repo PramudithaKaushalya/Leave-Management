835f8b7d892248d475296822bc225d71c52b158d
pramuditha <pramuditha@tsdclanka.com>
2021-07-01 16:46:20 +0530

    Remove NPM from git

---
+++ front_end/build/precache-manifest.bdb8ffa916c965155d7cf3d479c2084b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "04351a33eb1f49873e982c8b025d5718",
+    "url": "/static/media/error.04351a33.jpg"
+  },
+  {
+    "revision": "1c3a1d5412314537b9a2",
+    "url": "/static/css/main.43038dbe.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dbff4c93a1e5051ec7a24d438ac279d5",
+    "url": "/static/media/default_report.dbff4c93.gif"
+  },
+  {
+    "revision": "10bb1efe5348eb434b77",
+    "url": "/static/js/2.ade8c6cd.chunk.js"
+  },
+  {
+    "revision": "1c3a1d5412314537b9a2",
+    "url": "/static/js/main.21edf954.chunk.js"
+  },
+  {
+    "revision": "0a38972b4083f43522258cbe550a3cb7",
+    "url": "/static/media/404.0a38972b.png"
+  },
+  {
+    "revision": "94a09c58ff551455c1e841316770186a",
+    "url": "/static/media/bg.94a09c58.png"
+  },
+  {
+    "revision": "bbc97c266ce6c4050ea39b79d560a161",
+    "url": "/static/media/HRMS.bbc97c26.png"
+  },
+  {
+    "revision": "10bb1efe5348eb434b77",
+    "url": "/static/css/2.12903a8e.chunk.css"
+  },
+  {
+    "revision": "357980db86e14492e829c5a4c9b5cdce",
+    "url": "/index.html"
+  }
+];
